7a9a69c09092a8836a0dbe82d81af9866f7352f9
Marijan Brvar <marijan.brvar@gmail.com>
2021-07-05 15:31:49 +0200

    Add new book form and store to local storage feature.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const inpAuthor = document.getElementById('inpAuthor');
+const inpTitle = document.getElementById('inpTitle');
+const btnInsert = document.getElementById('btnInsert');
+const lsOutput = document.getElementById('lsOutput')
+
+btnInsert.onclick = function () {
+    const author = inpAuthor.value
+    const title = inpTitle.value
+
+    console.log(author, title);
+
+    if (author && title) {
+        localStorage.setItem(author, title)
+        // location.reload()
+    }
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const author = localStorage.key(i);
+        const title = localStorage.getItem(author)
+
+        lsOutput.innerHTML += `${author}: ${title} </br>` ;
+    }
+}
