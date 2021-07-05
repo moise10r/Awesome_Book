4de236200dd9c629c2dd66af353473e1bfff864c
moise10r <moiserushanika2006@gmail.com>
2021-07-05 17:28:48 +0200

    Fix Javascript linters.

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ const inpAuthor = document.getElementById('inpAuthor');
 const inpTitle = document.getElementById('inpTitle');
 const btnInsert = document.getElementById('btnInsert');
 const lsOutput = document.getElementById('lsOutput');
-const delBtn = document.getElementById('delBtn');
 
-let bookList = JSON.parse(localStorage.getItem('Books')) || [];
+const bookList = JSON.parse(localStorage.getItem('Books')) || [];
 
 btnInsert.onclick = function () {
 	const author = inpAuthor.value;
 	const title = inpTitle.value;
 
 	if (author && title) {
-		let newBook = { author: author, title: title };
+		const newBook = {
+			author,
+			title,
+		};
 		bookList.push(newBook);
 		localStorage.setItem('Books', JSON.stringify(bookList));
 		location.reload();
 	}
 };
 
+// eslint-disable-next-line no-unused-vars
 function removeBook(val) {
 	const book = bookList.filter((data, i) => i !== val);
 	localStorage.setItem('Books', JSON.stringify(book));
+	// eslint-disable-next-line no-restricted-globals
 	location.reload();
 }
 
