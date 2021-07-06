bd69f46184f087eea002c9e16bd909faff06ae40
Marijan Brvar <marijan.brvar@gmail.com>
2021-07-06 14:23:50 +0200

    Add diplayBook method to class and print results to console

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-restricted-globals */
-const inpAuthor = document.getElementById('inpAuthor');
-const inpTitle = document.getElementById('inpTitle');
-const btnInsert = document.getElementById('btnInsert');
-const lsOutput = document.getElementById('lsOutput');
+const inpAuthor = document.getElementById("inpAuthor");
+const inpTitle = document.getElementById("inpTitle");
+const btnInsert = document.getElementById("btnInsert");
+const lsOutput = document.getElementById("lsOutput");
 
-const bookList = JSON.parse(localStorage.getItem('Books')) || [];
+const bookList = JSON.parse(localStorage.getItem("Books")) || [];
 
 btnInsert.onclick = () => {
-	const author = inpAuthor.value;
-	const title = inpTitle.value;
+  const author = inpAuthor.value;
+  const title = inpTitle.value;
 
-	if (author && title) {
-		const newBook = {
-			author,
-			title,
-		};
-		bookList.push(newBook);
-		localStorage.setItem('Books', JSON.stringify(bookList));
-		location.reload();
-	}
+  if (author && title) {
+    const newBook = {
+      author,
+      title,
+    };
+    bookList.push(newBook);
+    localStorage.setItem("Books", JSON.stringify(bookList));
+    location.reload();
+  }
 };
 
 // eslint-disable-next-line no-unused-vars
 function removeBook(val) {
-	const book = bookList.filter((data, i) => i !== val);
-	localStorage.setItem('Books', JSON.stringify(book));
-	// eslint-disable-next-line no-restricted-globals
-	location.reload();
+  const book = bookList.filter((data, i) => i !== val);
+  localStorage.setItem("Books", JSON.stringify(book));
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
 }
 
 for (let i = 0; i < bookList.length; i += 1) {
-	const book = bookList[i];
-	lsOutput.innerHTML += `
+  const book = bookList[i];
+  lsOutput.innerHTML += `
   <button id="delBtn" onclick="removeBook(${i})">Delete</button>
-  ${'  '}
+  ${"  "}
   ${book.author}:
   ${book.title}
   </br>
@@ -42,20 +43,24 @@ for (let i = 0; i < bookList.length; i += 1) {
 
 // Book class Author and title
 class Book {
-	constructor(author, title) {
-		this.author = author;
-		this.title = title;
-	}
+  constructor(author, title) {
+    this.author = author;
+    this.title = title;
+  }
 }
 
 class UI {
-	static displayBook() {
-		const bookList = JSON.parse(localStorage.getItem('Books')) || [];
-		console.log(bookList);
-	}
+  static displayBook() {
+    const bookList = JSON.parse(localStorage.getItem('Books')) || [
+      new Book('J.R.Tolkin', 'L.O.T.R'),
+      new Book('J.R.Tolkin', 'L.O.T.R II'),
+    ];
+    return bookList;
+  }
 }
 
-UI.displayBook();
+const ui = UI.displayBook();
+console.log(ui);
 // Local storage to store the book
 // Display a book
 // Remove a book
